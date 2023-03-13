a7aaf670858a84c3cb351b8c8f4fbe4cac66edde
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2023-03-13 21:12:25 +0800

    Create Page.java

---
+++ NUMBER 5/Page.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class Page implements Turner {
+    public void turn(){
+        System.out.println("Page: Going to the next page");
+        System.out.println();
+    }
+    
+}
