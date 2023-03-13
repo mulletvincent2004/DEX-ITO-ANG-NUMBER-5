e4ae00e59938cc810fb9d45d18935275507995b1
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2023-03-13 21:11:35 +0800

    Create Demoturners.java

---
+++ NUMBER 5/Demoturners.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class DemoTurners {
+    public static void main(String[] args){
+        
+        Leaf l = new Leaf();
+        Page p = new Page();
+        Pancake pa = new Pancake();
+
+        l.turn();
+        p.turn();
+        pa.turn();
+
+    }
+}
