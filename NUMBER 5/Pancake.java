68f14a5403d41b9f5cb7bf6525ae696d66395698
Mullet, Vincent Jay R <111546395+mulletvincent2004@users.noreply.github.com>
2023-03-13 21:12:52 +0800

    Create Pancake.java

---
+++ NUMBER 5/Pancake.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class Pancake implements Turner {
+
+   public void turn() {
+        System.out.println("Pancake: Flipping");
+        System.out.println();
+    
+
+    }
+    
+}
